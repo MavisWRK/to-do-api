7744d27899a9d0965b0e7a7e897f426e35388dd1
william faisca <whitekine2000@gmail.com>
2026-01-15 13:46:53 -0300

    feat: imported fastify to the project and created the first route

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("ablublé");
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+fastify.get("/notes", async (request, reply) => {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
