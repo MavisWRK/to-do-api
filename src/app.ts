932fd2a1cf0c179744accade8a99403256c471d4
MavisWRK <10877g10877@gmail.com>
2026-01-15 17:01:18 -0300

    feat: created post route, note object

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,49 @@
 import Fastify from "fastify";
 
-const fastify = Fastify({
+const app = Fastify({
   logger: true,
 });
-fastify.get("/notes", async (request, reply) => {
-  return { hello: "world" };
+
+interface INote {
+  id: number;
+  title: string;
+  content: string;
+  category: string;
+}
+
+const notes: INote[] = [];
+
+app.get("/notes", async (request, reply) => {
+  return notes;
+});
+
+app.post("/notes", async (request, reply) => {
+  const body = request.body as {
+    title: string;
+    content: string;
+    category: string;
+  };
+
+  if (!body.title || !body.content || !body.category) {
+    return reply.status(400).send({
+      error: "Fill title, content and category!",
+    });
+  }
+
+  const newNote: INote = {
+    id: notes.length + 1,
+    title: body.title,
+    content: body.content,
+    category: body.category,
+  };
+
+  notes.push(newNote);
+  return newNote;
 });
 
 try {
-  await fastify.listen({ port: 3000 });
+  await app.listen({ port: 3000 });
 } catch (err) {
-  fastify.log.error(err);
+  app.log.error(err);
   process.exit(1);
 }
